e983ee2b727c565e95466c0f4bd30064c5503bae
Alexei <alxwtf@gmail.com>
2019-10-21 00:51:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
